7f7673e5a20d876bcc206862ca69b86695a52bf7
Tanay <tny.pndy@gmail.com>
2024-05-08 08:18:45 +0530

    first commit

---
+++ init/index.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const initdata=require("./data.js");
+const Listing=require("../models/listing.js");
+const MONGO_URL="mongodb://127.0.0.1:27017/wl";
+
+main()
+    .then(()=>{
+        console.log("connected to db")
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+const initDB=async () =>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initdata.data);
+    console.log("data was initialised");
+}
+initDB();
